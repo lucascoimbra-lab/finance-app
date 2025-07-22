329533dc4133247e50f947d7dba1c025ac17780a
Lucas Coimbra Scaglia <lucascoimbra88@hotmail.com>
2025-07-22 12:00:24 -0300

    criado enpoint test_connection para testar a conexão e manipulação do banco de dados com o backend

--- backend/index.js
+++ backend/index.js
@@ -1,9 +1,36 @@
-const express = require('express');
+import { Client } from 'pg'
+import express from 'express'
+
 const app = express();
 const port = 3000;
 
+const db_client = new Client({
+  user: 'postgres',
+  password: 'Postgress@tcc2025',
+  host: process.env.DB_HOST,
+  port: 5432,
+  database: 'financeapp',
+})
+
+await db_client.connect()
+
 app.get('/', (req, res) => {
   res.send('Olá, mundo com Express!');
+});
+
+app.get('/test_connection', async(req, res) => {
+  await db_client.query(`
+    UPDATE test_connection
+    SET counter = counter + 1
+    WHERE id = 1
+  `);
+
+  const result = await db_client.query(`
+    SELECT counter FROM test_connection WHERE id = 1
+  `);
+
+  res.send(result.rows[0]);
+
 });
 
 app.listen(port, () => {
