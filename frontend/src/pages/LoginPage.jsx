22bece2664a27088c205bd5d5689969c76618e70
Lucas Coimbra Scaglia <lucascoimbra88@hotmail.com>
2025-07-27 14:24:50 -0300

    endpoint check-email integrado na página de bem vindo, sendo chamado após clicar em enviar para validar o e-mail. neste momento, só valida se encontrou ou não.

--- frontend/src/pages/LoginPage.jsx
+++ frontend/src/pages/LoginPage.jsx
@@ -3,11 +3,35 @@ import logo from '../assets/logo.png';
 
 function LoginPage() {
   const [email, setEmail] = useState('');
+  const [message, setMessage] = useState('');
 
-  const handleSubmit = (e) => {
+  const handleSubmit = async (e) => {
     e.preventDefault();
-    console.log('E-mail submetido:', email);
-  };
+
+    try {
+      const response = await fetch('http://localhost:3000/check-email', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({ email }),
+      });
+
+      const data = await response.json();
+
+      if (data.isRegistered) {
+        alert('E-mail cadastrado.');
+      } else {
+        alert('E-mail não cadastrado.');
+      }
+    } catch (error) {
+      alert('Erro ao conectar com o servidor.');
+      console.error('Erro ao verificar e-mail:', error);
+  }
+};
+
+
+
 
   return (
     <div className="container">
